94e16ee988636443ad998a6b1efeeee78f2e13e3
Marie Manandise <marie@studio24.net>
2017-07-12 15:25:25 +0100

    sidebar secondary menu

--- web/wp-content/themes/wgcacsc/library/navigation.php
+++ web/wp-content/themes/wgcacsc/library/navigation.php
@@ -259,3 +259,157 @@ if ( ! function_exists( 'foundationpress_breadcrumb' ) ) {
 		echo '</ul>';
 	}
 }
+
+add_action('foundationpress_before_sidebar' , 's24_list_child_pages' , 10 , 0 );
+
+//list child pages of a section
+function s24_list_child_pages() {
+    //looks for current page in the main menu first
+    if ( ! (is_singular() || is_tax() || is_home() ) ) {
+        return;
+    }
+
+    //id of the page we are at
+    $current_object_id = 0;
+
+    global $post;
+
+    if (is_home() ) {
+        $current_object_id = get_option( 'page_for_posts' );
+        $current_object_type = 'post_type';
+    } elseif ( !empty($post) ) {
+        $current_object_id = $post->ID;
+        $current_object_type = 'post_type';
+    } elseif ( is_tax() ) {
+        $current_object_id = get_queried_object()->term_id;
+        $current_object_type = get_queried_object()->taxonomy;
+    }
+
+    if ( empty($current_object_id) ) {
+        //can't find this page in the system
+        return;
+    }
+
+    //Check whether it is in the main menu
+    $args = array(
+        'meta_query' => array(
+            'relationship' => 'AND',
+            array(
+                'key' => '_menu_item_object_id',
+                'value' => $current_object_id,
+                'compare' => '='
+            ),
+            array(
+                'key' => '_menu_item_type',
+                'value' => $current_object_type,
+                'compare' => '='
+            )
+        )
+    );
+
+    //we assume it is the topmost menu item for now - will fo up the hierarchy just after to get the topmost item
+    $top_menu_item = wp_get_nav_menu_items( 'main-menu' , $args);
+
+    if ( empty( $top_menu_item) ) {
+        return; //we could not find this in the main menu
+    }
+
+    $top_menu_item = $top_menu_item[0];
+
+    //if we are here, we found the current page in the main menu - we are going to build an array of menu items for the side menu
+
+    $side_menu_items = array(); //each item has 'item' = OBJ the menu item, 'children' = ARRAY children menu items
+
+    //let's find the topmost menu item
+    if ( !empty( $top_menu_item->menu_item_parent ) ) {
+        //get the topmost menu item
+        while ( !empty($top_menu_item->menu_item_parent) ) {
+            $args = array(
+                'posts_per_page' => 1,
+                'p' => $top_menu_item->menu_item_parent
+            );
+            $top_menu_item = wp_get_nav_menu_items('main-menu', $args);
+            $top_menu_item = $top_menu_item[0];
+        }
+    }
+
+    array_push( $side_menu_items , s24_menu_item_array( $top_menu_item ) );
+
+    //builds the menu html
+    if ( empty($side_menu_items[0]['children']) ) {
+        return; //no menu items except the current page
+    }
+
+    //menu html
+    $menu_string = '<article class="widget widget_nav_menu s24-side-menu"><div class="side-menu-container">';
+    $menu_string .= s24_build_menu_list( $side_menu_items[0] , $current_object_id , true );
+    $menu_string .= '</div></article>';
+    echo $menu_string;
+}
+
+//creates and returns menu item array (item itself and children - recursively )
+function s24_menu_item_array( $root_menu_item ) {
+    $menu_item_array = array();
+    $menu_item_array['item'] = s24_set_single_menu_item($root_menu_item);
+    $menu_item_array['children'] = array();
+
+    //get children
+    $args = array(
+        'meta_query' => array(
+            array(
+                'key' => '_menu_item_menu_item_parent',
+                'value' => $root_menu_item->ID,
+                'compare' => '='
+            )
+        )
+    );
+    $children_item = wp_get_nav_menu_items( 'main-menu' , $args );
+
+    if ( !empty($children_item) ) {
+        foreach ( $children_item as $child_item ) {
+            array_push( $menu_item_array['children'] , s24_menu_item_array( $child_item ) );
+        }
+    }
+
+    return $menu_item_array;
+}
+
+function s24_set_single_menu_item( $menu_item ) {
+    $items = array($menu_item);
+    $items = array_map( 'wp_setup_nav_menu_item', $items );
+    $items = apply_filters( 'wp_get_nav_menu_items', $items );
+    return $items[0];
+}
+
+function s24_build_menu_list( $side_menu_item , $current_id , $topmost = false ) {
+
+    $list_html = '';
+    $item_object = $side_menu_item['item'];
+    $submenu_class = 'sub-menu';
+    if ( $topmost ) {
+        //topmost item
+        $list_html .= '<h6><a href="'.$item_object->url.'">'.$item_object->title.'</a></h6>';
+        $submenu_class = 'menu';
+    } else {
+        $li_classes = '';
+        if ( $item_object->object_id == $current_id ) {
+            $li_classes = ' class="current-menu-item"';
+        }
+        $list_html .= '<li'.$li_classes.'><a href="'.$item_object->url.'">'.$item_object->title.'</a>';
+    }
+
+    //children ul
+    if ( !empty($side_menu_item['children'] ) ) {
+        $list_html .= '<ul class="'.$submenu_class.'">';
+        foreach ( $side_menu_item['children'] as $child_item ) {
+            $list_html .= s24_build_menu_list( $child_item , $current_id );
+        }
+        $list_html .= '</ul>';
+    }
+
+    if ( !$topmost ) {
+        $list_html .= '</li>';
+    }
+
+    return $list_html;
+}
