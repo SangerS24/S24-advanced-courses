63a299e26fcf8d8155d2abb178e478e09c17d193
Marie Manandise <marie@studio24.net>
2017-07-14 14:27:10 +0100

    event custom post type

--- web/wp-content/themes/wgcacsc/library/custom-post-types.php
+++ web/wp-content/themes/wgcacsc/library/custom-post-types.php
@@ -3,42 +3,39 @@
 add_action( 'init', 'wgcacsc_register_my_cpts' );
 function wgcacsc_register_my_cpts()
 {
-//    // Event custom post type
-//    $labels = array(
-//        "name" => __('Events', ''),
-//        "singular_name" => __('Event', ''),
-//        "menu_name" => __('Events', ''),
-//        "name_admin_bar" => __('Event', ''),
-//        'add_new'            => __('Add New', 'event', ''),
-//        'add_new_item'       => __('Add New Event', ''),
-//        'new_item'           => __('New Event', ''),
-//        'edit_item'          => __('Edit Event', ''),
-//        'view_item'          => __('View Event', ''),
-//        'all_items'          => __('All Events', ''),
-//        'search_items'       => __('Search Events', ''),
-//        'parent_item_colon'  => __('Parent Events:', ''),
-//        'not_found'          => __('No events found.', ''),
-//        'not_found_in_trash' => __('No events found in Trash.', '')
-//    );
-//
-//    $args = array(
-//        "labels" => $labels,
-//        "description" => "",
-//        "public" => true,
-//        "show_ui" => true,
-//        "show_in_rest" => false,
-//        "rest_base" => "",
-//        "has_archive" => false,
-//        "show_in_menu" => true,
-//        "exclude_from_search" => false,
-//        "capability_type" => "post",
-//        "map_meta_cap" => true,
-//        "hierarchical" => false,
-//        "rewrite" => array("slug" => "events"),
-//        "query_var" => true,
-//        "menu_icon" => "dashicons-calendar-alt",
-//        "supports" => array("title", "excerpt", "revisions", "author"),
-//    );
-//    register_post_type("event", $args);
+    // Event custom post type
+    $labels = array(
+        "name" => __('Events', ''),
+        "singular_name" => __('Event', ''),
+        "menu_name" => __('Events', ''),
+        "name_admin_bar" => __('Event', ''),
+        'add_new'            => __('Add New', 'event', ''),
+        'add_new_item'       => __('Add New Event', ''),
+        'new_item'           => __('New Event', ''),
+        'edit_item'          => __('Edit Event', ''),
+        'view_item'          => __('View Event', ''),
+        'all_items'          => __('All Events', ''),
+        'search_items'       => __('Search Events', ''),
+        'parent_item_colon'  => __('Parent Events:', ''),
+        'not_found'          => __('No events found.', ''),
+        'not_found_in_trash' => __('No events found in Trash.', '')
+    );
+
+    $args = array(
+        "labels" => $labels,
+        "description" => "",
+        "public" => true,
+        "show_ui" => true,
+        "has_archive" => true,
+        "show_in_menu" => true,
+        "exclude_from_search" => false,
+        "capability_type" => "post",
+        "map_meta_cap" => true,
+        "rewrite" => array("slug" => "events"),
+        "query_var" => true,
+        "menu_icon" => "dashicons-calendar-alt",
+        "supports" => array("title", "excerpt", "revisions", "author"),
+    );
+    register_post_type("event", $args);
 
 }
