d1a8995174e50ebf9257518de3ab5e7a3d0f2941
Marie Manandise <marie@studio24.net>
2017-07-15 14:36:20 +0100

    bio component markup and some css

--- web/wp-content/themes/wgcacsc/fewbricks/bricks/component-bio.php
+++ web/wp-content/themes/wgcacsc/fewbricks/bricks/component-bio.php
@@ -24,11 +24,13 @@ class component_bio extends project_brick
     public function set_fields()
     {
 
-        $this->add_repeater( (new acf_fields\repeater( 'Staff bios' , 'bios' , '201707141712a' ))
-        ->add_sub_field( new acf_fields\image( 'Picture' , 'bio_picture' , '201707141713a' ) )
-        ->add_sub_field( new acf_fields\text( 'Name' , 'bio_name' , '201707141714a'))
-        ->add_sub_field( new acf_fields\text( 'Job title' , 'bio_job_title' , '201707141715a'))
-        ->add_sub_field( new acf_fields\wysiwyg( 'Bio' , 'bio_bio' , '201707141715b' , [
+        $this->add_repeater( (new acf_fields\repeater( 'Staff bios' , 'staff_bios' , '201707151148' , [
+            'button_label' => 'Add staff'
+        ] ))
+        ->add_sub_field( new acf_fields\image( 'Picture' , 'picture' , '201707141713a' ) )
+        ->add_sub_field( new acf_fields\text( 'Name' , 'name' , '201707141714a'))
+        ->add_sub_field( new acf_fields\text( 'Job title' , 'job_title' , '201707141715a'))
+        ->add_sub_field( new acf_fields\wysiwyg( 'Bio' , 'bio' , '201707141715b' , [
             'toolbar' => 'basic'
         ])));
 
@@ -39,21 +41,51 @@ class component_bio extends project_brick
      */
     public function get_brick_html($args = array())
     {
+        $html = '';
 
-        $html = '<div class="component-bio">';
+//        $bios = $this->get_field('staff_bios');
 
-        $bios = $this->get_field('bios');
+        if( $this->have_rows( 'staff_bios' ) ) {
+            $html .= '<div class="component component-bio offset-content">';
 
-        if( !empty($bios) ) {
-//            $html .= '<div class="staff-bio">';
-//            $html .= '<div class="marker" data-lat="' . $map["lat"] . '" data-lng="' . $map["lng"] . '"></div>';
-//            $html .= '</div>';
+            while ( $this->have_rows( 'staff_bios' ) ) {
+                $this->the_row();
+
+                $staff_picture = $this->get_field_in_repeater( 'staff_bios' , 'picture');
+                if ( !empty($staff_picture) ) {
+                    $staff_picture_src_array = wp_get_attachment_image_src( $staff_picture , 'bio-listing' );
+                    $staff_picture_src = $staff_picture_src_array[0];
+                } else {
+                    $staff_picture_src = get_template_directory_uri().'/assets/images/headshot_placeholder.svg';
+                }
+                $staff_name = $this->get_field_in_repeater( 'staff_bios' , 'name' );
+                $staff_title = $this->get_field_in_repeater( 'staff_bios' , 'job_title' );
+                $staff_bio = apply_filters( 'the_content' , $this->get_field_in_repeater( 'staff_bios' , 'bio') );
+
+                $html .= '<div class="component-bio__staff">';
+                $html .= '<div class="component-bio__staff__picture"><img src="'.$staff_picture_src.'" alt="'.$staff_name.'" /></div>';
+                $html .= '<div class="component-bio__staff__text-content">';
+                if ( !empty( $staff_name) ) {
+                    $html .= '<h4>'.$staff_name.'</h4>';
+                }
+                if ( !empty( $staff_title ) ) {
+                    $html .= '<p class="component-bio__staff__text-content__job-title">'.$staff_title.'</p>';
+                }
+                if ( !empty( $staff_bio ) ) {
+                    $html .= $staff_bio;
+                }
+                $html .= '</div>';
+                $html .= '</div>';
+            }
+
+
+            $html .= '</div>';
         }
 
-        $html .= '</div>';
 
         return $html;
 
     }
 
 }
+?>
