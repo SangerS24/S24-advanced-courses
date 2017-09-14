ae31f1e45086b337c2aa4b150e19d64e58119e5d
Marie Manandise <marie@studio24.net>
2017-09-14 11:31:53 +0100

    don't change URL when clicking collapsible

--- web/wp-content/themes/wgcacsc/assets/javascript/custom/accordion-linking.js
+++ web/wp-content/themes/wgcacsc/assets/javascript/custom/accordion-linking.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
     //change URL when accordion opened
-    jQuery('.accordion-title').click( function(e){
-        var hash = jQuery(this).attr('href');
-        history.pushState( 'page-section' , '' , hash );
-    });
+    // jQuery('.accordion-title').click( function(e){
+    //     var hash = jQuery(this).attr('href');
+    //     history.pushState( 'page-section' , '' , hash );
+    // });
 
     //go to panel if in URL
     if ( window.location.hash ) {
