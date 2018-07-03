923faf26e69ec784e9da2e71de6b9aa6b07dbff1
Carlos Eriksson <carlos@studio24.net>
2018-07-03 10:57:05 +0100

    Update search icon

--- web/wp-content/themes/wgcacsc/searchform.php
+++ web/wp-content/themes/wgcacsc/searchform.php
@@ -14,6 +14,7 @@ do_action( 'foundationpress_before_searchform' ); ?>
 		<?php do_action( 'foundationpress_searchform_before_search_button' ); ?>
 		<div class="input-group-button search-form__input-group-button">
 			<button id="searchsubmit" class="button search__submit">
+                <span><svg width="18" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M9.8 12.8a6.8 6.8 0 1 1 2.5-2.2l4.8 4.7s.8.8 0 1.6l-.8.7c-.8.8-1.6 0-1.6 0l-4.9-4.8zm-7.5-6a4.5 4.5 0 1 0 9 0 4.5 4.5 0 0 0-9 0z" fill-rule="nonzero" fill="currentColor"/></svg></span>
 				<span class="show-for-sr search__submit__text"><?php esc_attr_e( 'Search', 'foundationpress' ); ?></span>
 			</button>
 		</div>
