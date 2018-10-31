cece0b9c4ddbb78496b10516d99c2e63c2709c54
Carlos Eriksson <carlos@studio24.net>
2018-10-31 14:05:48 +0000

    Update plugins

--- web/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ web/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit56290c8d7c9e66f191342ce4e3fd0017::getLoader();
+return ComposerAutoloaderInitf54682a203dcdd18cee206e5e917907a::getLoader();
